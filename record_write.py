9ea6ca7db263af89e6d2e9c51f4712d82d2863d0
Vincent Cuenca <vincent.e.cuenca@gmail.com>
2017-01-22 18:00:19 -0500

    update data script

--- record_write.py
+++ record_write.py
@@ -1,9 +1,10 @@
 from subprocess import check_output
 
-byte_sizes = [100, 1000, 10000, 100000, 1000000, 1500000, 2048000, 2500000, 2800000, 3000000]
-total_bytes = [100, 1000, 10000, 100000, 1000000, 10000000]
+block_sizes = [100, 1000, 10000, 100000, 1000000, 1500000, 2000000, 2500000, 2800000, 3000000]
+total_bytes = [100000000]
 
+print "write rate,block size"
 for total in total_bytes:
-  for size in byte_sizes:
-    out = check_output(["./create_random_file", "output.txt", str(total), str(size)])
-    print(out, total, size)
+  for block_size in block_sizes:
+    out = check_output(["./create_random_file", "output.txt", str(total), str(block_size)])
+    print(str(total / int(out)) + "," + str(block_size))
