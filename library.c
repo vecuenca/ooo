85aea13021eaeec4570d9a705efc8c51a4fe4a7c
Jack Yiu <jackinyiu@gmail.com>
2017-01-22 17:24:32 -0500

    Updated getTime function and random_array

--- library.c
+++ library.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <sys/time.h>
+#include <sys/timeb.h>
 
 long getTime()
 {
-	struct timeval currentTime;
-	gettimeofday(&currentTime, NULL);
-	return currentTime.tv_sec * (int)1e6 + currentTime.tv_usec;	
+        struct timeb t;
+        ftime(&t);
+        
+	return t.time * 1000 + t.millitm;
 }
 
 void random_array(char *array, long bytes)
 {
-	char c;	
-	for (long i = 0; i < bytes; i++) {
+	char c;
+	long i;
+	
+	for (i = 0; i < bytes; i++) {
 		c = 'A' + (rand() % 26);	
 		array[i] = c;
 	}
