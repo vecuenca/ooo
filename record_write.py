b2fba5e4b3795f1a67cfbcbac85db38c9658ee39
Vincent Cuenca <vincent.e.cuenca@gmail.com>
2017-01-25 19:18:17 -0500

    list comprehension is cool

--- record_write.py
+++ record_write.py
@@ -1,9 +1,12 @@
 from subprocess import check_output
 
-block_sizes = [100, 1000, 10000, 100000, 1000000, 1500000, 2000000, 2500000, 2800000, 3000000]
+
+block_sizes = [2**x for x in range(5, 22)] + [3145728] # max block size is 3MB
+print block_sizes
+# block_sizes = [100, 1000, 10000, 100000, 1000000, 1500000, 2000000, 2500000, 2800000, 3000000]
 total_bytes = [100000000]
 
-# file = "/media/vincent/LEXAR/oo"
+file = "/media/vincent/LEXAR/oo"
 file = "output.txt"
 
 print "write rate,block size"
