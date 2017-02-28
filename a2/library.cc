ae3a371ab3e2a95bdc4c6ffde51aa87dcb4aad98
Yu Ting Jack Yiu <yiuyu@IITS-B473-07.utsc-labs.utoronto.ca>
2017-02-28 16:24:05 -0500

    Fixed Q3 compilation errors

--- a2/library.cc
+++ a2/library.cc
@@ -1,11 +1,18 @@
 // LICENSE: hey no use for skool work
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
 #include <string.h>
 typedef const char* V;
 typedef std::vector<V> Record;
+
+typedef struct {
+    void *data;
+    int page_size;
+    int slot_size;
+} Page;
 
 // We assume that there is only one table schema. 
 // 100 attributes, each attribute is 10 bytes each. 
@@ -61,7 +68,7 @@ int fixed_len_page_capacity(Page *page) {
  * Calculate the number of free slots in the page
  */
 int fixed_len_page_freeslots(Page *page) {
-	char *data = page->data;
+	char *data = (char *) page->data;
 	int num_free_slots = 0;
 	for (int i = 0; i < fixed_len_page_capacity(page); i++) {
 		if (data[i * page->slot_size] == '\0') {
@@ -69,6 +76,33 @@ int fixed_len_page_freeslots(Page *page) {
 		}
 	}
 	return num_free_slots;
+}
+
+
+/**
+ * Write a record into a given slot.
+ */
+int write_fixed_len_page(Page *page, int slot, Record *r) {
+	if (fixed_len_page_freeslots(page) < 1) {
+		return -1;
+	}
+
+	int slot_offset = slot * page->slot_size;
+	if (((char *) page->data)[slot_offset] == '\0') {
+		// create buf to read record into
+		char* record_buf = (char *) malloc(fixed_len_sizeof(r));
+
+		// null terminate buf to ensure no problems
+		memset(record_buf, '\0', sizeof(char) * fixed_len_sizeof(r));
+		fixed_len_write(r, record_buf);
+		memcpy(page->data, record_buf, fixed_len_sizeof(r));
+
+		free(record_buf);
+
+		return slot_offset;
+	} else {
+		return -1;
+	}
 }
 
 /**
@@ -84,33 +118,7 @@ int add_fixed_len_page(Page *page, Record *r) {
 	// find first available slot?
 	for (int i = 0; i < fixed_len_page_capacity(page); i++) {
 		int slot_offset = i * page->slot_size;
-		write_fixed_len_page(page, i, r)
+		write_fixed_len_page(page, i, r);
 	}
 	return -1;
 }
-
-/**
- * Write a record into a given slot.
- */
-int write_fixed_len_page(Page *page, int slot, Record *r) {
-	if (fixed_len_page_freeslots(page) < 1) {
-		return -1;
-	}
-
-	int slot_offset = slot * page->slot_size;
-	if (data[slot_offset] == '\0') {
-		// create buf to read record into
-		char[] record_buf = new char[fixed_len_sizeof(r)];
-
-		// null terminate buf to ensure no problems
-		memset(record_buf, '\0', sizeof(char) * fixed_len_sizeof(r));
-		fixed_len_write(r, record_buf);
-		memcpy(page->data, record_buf, fixed_len_sizeof(r));
-
-		return slot_offset;
-	} else {
-		return -1;
-	}
-}
-
-int write_to_
