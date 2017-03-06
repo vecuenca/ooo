d22e46b405c3b5a92ed1f5fe93343fd2f59f0e18
Vincent Cuenca <vincent.e.cuenca@gmail.com>
2017-03-06 15:53:42 -0500

    randomize data

--- a2/sample_record.py
+++ a2/sample_record.py
@@ -1,9 +1,10 @@
+from random import randint
+
 import string
 file = open('samplerecord.csv', 'w+')
-alphabet = string.lowercase
-for i in range(1000):
+for i in range(2000):
 	row = ''
 	for j in range(100):
-		row += string.lowercase[j % 26] * 10 + ','
+		row += string.lowercase[randint(0,25)] * 10 + ','
 	row = row[:-1]
 	file.write(row + '\n')
